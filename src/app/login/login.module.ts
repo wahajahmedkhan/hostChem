25942d87866c5336a0e7921a4959cf21bd4b1a9c
Wahaj Ahmed Khan <wahajahmedkhan95@gmail.com>
2020-02-20 00:25:17 +0500

    Login Done

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {LoginComponent} from './login.component';
+
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+      FormsModule,
+      IonicModule,
+      RouterModule.forChild([
+          {
+              path: '',
+              component: LoginComponent
+          }
+      ])
+  ]
+})
+export class LoginModule { }
